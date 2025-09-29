9a8ca0b4932590401ebc17e997fb4e30806c0166
Dr Umran Ali <64030560+DrUmranAli@users.noreply.github.com>
2025-09-29 15:07:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+
+    mobileMenuToggle.addEventListener('click', function() {
+        mainNav.classList.toggle('active');
+        mobileMenuToggle.classList.toggle('active');
+    });
+});
